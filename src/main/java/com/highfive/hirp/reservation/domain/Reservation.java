ea6eb42908ee3210e901affafc2682a73b80c3f3
jinsil <developer_corn@daum.net>
2022-04-17 01:35:18 +0900

    DTO 코드 추가

--- src/main/java/com/highfive/hirp/reservation/domain/Reservation.java
+++ src/main/java/com/highfive/hirp/reservation/domain/Reservation.java
@@ -1,5 +1,91 @@
 package com.highfive.hirp.reservation.domain;
 
-public class Reservation {
+import java.sql.Date;
 
+public class Reservation {
+	private int reservationNo;
+	private String emplId;
+	private int utilityNo;
+	private Date reservationStartDate;
+	private Date reservationEndDate;
+	private String reservationConts;
+	private String isReturn;
+
+	public Reservation() {
+	}
+
+	public Reservation(int reservationNo, String emplId, int utilityNo, Date reservationStartDate,
+			Date reservationEndDate, String reservationConts, String isReturn) {
+		super();
+		this.reservationNo = reservationNo;
+		this.emplId = emplId;
+		this.utilityNo = utilityNo;
+		this.reservationStartDate = reservationStartDate;
+		this.reservationEndDate = reservationEndDate;
+		this.reservationConts = reservationConts;
+		this.isReturn = isReturn;
+	}
+
+	public int getReservationNo() {
+		return reservationNo;
+	}
+
+	public void setReservationNo(int reservationNo) {
+		this.reservationNo = reservationNo;
+	}
+
+	public String getEmplId() {
+		return emplId;
+	}
+
+	public void setEmplId(String emplId) {
+		this.emplId = emplId;
+	}
+
+	public int getUtilityNo() {
+		return utilityNo;
+	}
+
+	public void setUtilityNo(int utilityNo) {
+		this.utilityNo = utilityNo;
+	}
+
+	public Date getReservationStartDate() {
+		return reservationStartDate;
+	}
+
+	public void setReservationStartDate(Date reservationStartDate) {
+		this.reservationStartDate = reservationStartDate;
+	}
+
+	public Date getReservationEndDate() {
+		return reservationEndDate;
+	}
+
+	public void setReservationEndDate(Date reservationEndDate) {
+		this.reservationEndDate = reservationEndDate;
+	}
+
+	public String getReservationConts() {
+		return reservationConts;
+	}
+
+	public void setReservationConts(String reservationConts) {
+		this.reservationConts = reservationConts;
+	}
+
+	public String getIsReturn() {
+		return isReturn;
+	}
+
+	public void setIsReturn(String isReturn) {
+		this.isReturn = isReturn;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [reservationNo=" + reservationNo + ", emplId=" + emplId + ", utilityNo=" + utilityNo
+				+ ", reservationStartDate=" + reservationStartDate + ", reservationEndDate=" + reservationEndDate
+				+ ", reservationConts=" + reservationConts + ", isReturn=" + isReturn + "]";
+	}
 }
--- src/main/java/com/highfive/hirp/schedule/domain/Schedule.java
+++ src/main/java/com/highfive/hirp/schedule/domain/Schedule.java
@@ -1,5 +1,112 @@
 package com.highfive.hirp.schedule.domain;
 
-public class Schedule {
+import java.sql.Date;
 
+public class Schedule {
+	private int scheduleNo;
+	private String emplId;
+	private String scheduleCategory;
+	private String scheduleColor;
+	private String scheduleConts;
+	private Date scheduleStartDate;
+	private Date scheduleEndDate;
+	private String schedulePlace;
+	private String scheduleAlarm;
+
+	public Schedule() {
+	}
+
+	public Schedule(int scheduleNo, String emplId, String scheduleCategory, String scheduleColor, String scheduleConts,
+			Date scheduleStartDate, Date scheduleEndDate, String schedulePlace, String scheduleAlarm) {
+		super();
+		this.scheduleNo = scheduleNo;
+		this.emplId = emplId;
+		this.scheduleCategory = scheduleCategory;
+		this.scheduleColor = scheduleColor;
+		this.scheduleConts = scheduleConts;
+		this.scheduleStartDate = scheduleStartDate;
+		this.scheduleEndDate = scheduleEndDate;
+		this.schedulePlace = schedulePlace;
+		this.scheduleAlarm = scheduleAlarm;
+	}
+
+	public int getScheduleNo() {
+		return scheduleNo;
+	}
+
+	public void setScheduleNo(int scheduleNo) {
+		this.scheduleNo = scheduleNo;
+	}
+
+	public String getEmplId() {
+		return emplId;
+	}
+
+	public void setEmplId(String emplId) {
+		this.emplId = emplId;
+	}
+
+	public String getScheduleCategory() {
+		return scheduleCategory;
+	}
+
+	public void setScheduleCategory(String scheduleCategory) {
+		this.scheduleCategory = scheduleCategory;
+	}
+
+	public String getScheduleColor() {
+		return scheduleColor;
+	}
+
+	public void setScheduleColor(String scheduleColor) {
+		this.scheduleColor = scheduleColor;
+	}
+
+	public String getScheduleConts() {
+		return scheduleConts;
+	}
+
+	public void setScheduleConts(String scheduleConts) {
+		this.scheduleConts = scheduleConts;
+	}
+
+	public Date getScheduleStartDate() {
+		return scheduleStartDate;
+	}
+
+	public void setScheduleStartDate(Date scheduleStartDate) {
+		this.scheduleStartDate = scheduleStartDate;
+	}
+
+	public Date getScheduleEndDate() {
+		return scheduleEndDate;
+	}
+
+	public void setScheduleEndDate(Date scheduleEndDate) {
+		this.scheduleEndDate = scheduleEndDate;
+	}
+
+	public String getSchedulePlace() {
+		return schedulePlace;
+	}
+
+	public void setSchedulePlace(String schedulePlace) {
+		this.schedulePlace = schedulePlace;
+	}
+
+	public String getScheduleAlarm() {
+		return scheduleAlarm;
+	}
+
+	public void setScheduleAlarm(String scheduleAlarm) {
+		this.scheduleAlarm = scheduleAlarm;
+	}
+
+	@Override
+	public String toString() {
+		return "Schedule [scheduleNo=" + scheduleNo + ", emplId=" + emplId + ", scheduleCategory=" + scheduleCategory
+				+ ", scheduleColor=" + scheduleColor + ", scheduleConts=" + scheduleConts + ", scheduleStartDate="
+				+ scheduleStartDate + ", scheduleEndDate=" + scheduleEndDate + ", schedulePlace=" + schedulePlace
+				+ ", scheduleAlarm=" + scheduleAlarm + "]";
+	}
 }
