2744e27a3c2688503e10d966faed751d9b925fcf
ming <devel.moon17@gmail.com>
2022-04-19 12:43:03 +0900

    survey 1차 push

--- src/main/java/com/highfive/hirp/survey/controller/SurveyController.java
+++ src/main/java/com/highfive/hirp/survey/controller/SurveyController.java
@@ -1,8 +1,20 @@
 package com.highfive.hirp.survey.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.highfive.hirp.survey.domain.Survey;
+import com.highfive.hirp.survey.domain.SurveyQuest;
+import com.highfive.hirp.survey.domain.SurveyQuestCh;
 import com.highfive.hirp.survey.service.SurveyService;
 
 @Controller
@@ -10,4 +22,74 @@ public class SurveyController {
 	@Autowired
 	private SurveyService sService;
 	
+	//설문조사 메인페이지 (최신 리스트 조회)
+	public ModelAndView surveyMain(ModelAndView mv) {
+		return mv;
+	}
+	
+	//진행중인 설문 페이지 (리스트 조회)
+	public ModelAndView ProceedSurvey(ModelAndView mv) {
+		return mv;
+	}
+	
+	//마감된 설문 페이지 (리스트 조회)
+	public ModelAndView closedSurvey(ModelAndView mv) {
+		return mv;
+	}
+
+	//내가 만든 설문 페이지 (리스트 조회)
+	public ModelAndView wroteSurvey(ModelAndView mv) {
+		return mv;
+	}
+	
+	
+	//설문 등록 (설문정보, 문항까지 저장 임시저장여부도 가져와서 넣어주기)
+	public ModelAndView writeSurvey(ModelAndView mv
+			,@ModelAttribute Survey survey
+			,@ModelAttribute SurveyQuest surveyQuest1
+			,@ModelAttribute SurveyQuest surveyQuest2
+			,@ModelAttribute SurveyQuest surveyQuest3
+			,@ModelAttribute SurveyQuest surveyQuest4
+			,@ModelAttribute SurveyQuestCh qCh1
+			,@ModelAttribute SurveyQuestCh qCh2
+			,@ModelAttribute SurveyQuestCh qCh3
+			,@ModelAttribute SurveyQuestCh qCh4
+			, HttpServletRequest request) {
+		return mv;
+	}
+	//대상자 리스트 가져오기 
+	public ModelAndView chooseEmpl(ModelAndView mv) {
+		return mv;
+	}
+	
+	//설문 수정
+	public ModelAndView surveyModify(ModelAndView mv) {
+		return mv;
+	}
+	//설문 마감
+	public ModelAndView surveyClose(ModelAndView mv) {
+		return mv;
+	}
+	//설문 삭제
+	public ModelAndView surveyDelete(ModelAndView mv) {
+		return mv;
+	}
+	
+	
+	//설문 응답 페이지 (설문 상세1)
+	public ModelAndView surveySubmitPage(ModelAndView mv) {
+		return mv;
+	}
+	//설문 응답 제출
+	public ModelAndView surveySubmit(ModelAndView mv) {
+		return mv;
+	}
+	//설문 응답 수정
+	public ModelAndView surveySubmitModify(ModelAndView mv) {
+		return mv;
+	}
+	//설문 결과 페이지 (설문 상세2)
+	public ModelAndView surveySubmitResult(ModelAndView mv) {
+		return mv;
+	}
 }
--- src/main/java/com/highfive/hirp/survey/store/logic/SurveyStoreLogic.java
+++ src/main/java/com/highfive/hirp/survey/store/logic/SurveyStoreLogic.java
@@ -1,11 +1,141 @@
 package com.highfive.hirp.survey.store.logic;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.highfive.hirp.survey.domain.Survey;
+import com.highfive.hirp.survey.domain.SurveyAnswer;
+import com.highfive.hirp.survey.domain.SurveyQuest;
+import com.highfive.hirp.survey.domain.SurveyQuestCh;
+import com.highfive.hirp.survey.domain.SurveySub;
+import com.highfive.hirp.survey.domain.SurveySubUpdate;
 import com.highfive.hirp.survey.store.SurveyStore;
 
 @Repository
 public class SurveyStoreLogic implements SurveyStore{
+
+	//설문조사 리스트 조회
+	//전체 리스트 조회(최신)
+	@Override
+	public List<Survey> selectAllSurvey(SqlSession sqlSession) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//진행중인 리스트 조회
+	@Override
+	public List<Survey> selectProceedSurvey(SqlSession sqlSession) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//마감된 설문리스트 조회
+	@Override
+	public List<Survey> selectClosedSurvey(SqlSession sqlSession) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//내가 작성한 설문 리스트 조회
+	@Override
+	public List<Survey> selectWroteSurvey(SqlSession sqlSession) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//내가 대상자인 것 중 진행 중이면서 응답하지 않은 설문 리스트 조회
+	@Override
+	public List<Survey> selectSubSurveyById(SqlSession sqlSession, String emplId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//설문조사 대상자 가져오기 (응답여부 확인 가능)
+	@Override
+	public List<SurveySub> selectSurveySubByNo(SqlSession sqlSession, int surveyNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//설문조사 등록
+	//설문 추가
+	@Override
+	public int insertSurvey(SqlSession sqlSession, Survey survey) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//설문 문항 추가
+	@Override
+	public int insertSurveyQuest(SqlSession sqlSession, SurveyQuest surveyQuest) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//설문 보기 추가 (날짜/객관식의 경우)
+	@Override
+	public int insertSurveyQuestCh(SqlSession sqlSession, SurveyQuestCh qCh) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//설문 대상자 리스트 추가
+	@Override
+	public int insertSurveySub(SqlSession sqlSession, List<SurveySub> subList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//설문조사 상세
+	//설문조사 정보 가져오기
+	@Override
+	public Survey selectSurveyByNo(SqlSession sqlSession, int surveyNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//설문조사에 포함된 설문 문항 모두 가져오기
+	@Override
+	public List<SurveyQuest> selectSurveyQuestByNo(SqlSession sqlSession, int surveyNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//설문조사 보기 가져오기
+	@Override
+	public SurveyQuestCh selectSurveyQuestChByNo(SqlSession sqlSession, int surveyQuestNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//설문조사 수정
+	//설문조사 정보 수정
+	@Override
+	public int updateSurvey(SqlSession sqlSession, Survey survey) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//설문조사 대상자 리스트 수정
+	@Override
+	public int updateSurveySubList(SqlSession sqlSession, List<SurveySub> subList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//설문조사 응답
+	//설문조사 응답 내용 추가
+	@Override
+	public int insertSurveySubAnswer(SqlSession sqlSession, SurveyAnswer surveyAnswer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//설문조사 응답자 응답상태 변경
+	@Override
+	public int updateSubAnswerStatus(SqlSession sqlSession, SurveySubUpdate ssUpdate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	//설문조사 응답 수정
+	@Override
+	public int updateSurveySubAnswer(SqlSession sqlSession, SurveyAnswer surveyAnswer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	//설문조사 검색
 	
 }
