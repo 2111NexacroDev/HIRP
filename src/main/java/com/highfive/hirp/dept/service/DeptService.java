905fe9abfa3354ff1f2e28a3d4545d9ea24e64d0
ming <devel.moon17@gmail.com>
2022-04-19 17:41:34 +0900

    dept 클래스 끝

--- src/main/java/com/highfive/hirp/dept/service/DeptService.java
+++ src/main/java/com/highfive/hirp/dept/service/DeptService.java
@@ -1,5 +1,25 @@
 package com.highfive.hirp.dept.service;
 
-public interface DeptService {
+import java.util.List;
 
+import com.highfive.hirp.dept.domain.Dept;
+
+public interface DeptService {
+	//부서 리스트 가져오기
+	public List<Dept> selectAllDept();
+	
+	//부서 선택해서 부서 정보 가져오기
+	public Dept selectDeptInfo(String deptCode);
+	
+	//부서 이름 검색 부서 리스트 가져오기
+	public List<Dept> selectDeptSearch(String deptSearchName);
+	
+	//부서 추가
+	public int insertDept(Dept dept);
+	
+	//부서 정보 수정
+	public int updateDept(Dept dept);
+	
+	//부서 삭제
+	public int deleteDept(String deptCode);
 }
--- src/main/java/com/highfive/hirp/dept/store/logic/DeptStoreLogic.java
+++ src/main/java/com/highfive/hirp/dept/store/logic/DeptStoreLogic.java
@@ -1,10 +1,55 @@
 package com.highfive.hirp.dept.store.logic;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.highfive.hirp.dept.domain.Dept;
 import com.highfive.hirp.dept.store.DeptStore;
 
 @Repository
 public class DeptStoreLogic implements DeptStore{
 
+	//부서 리스트 가져오기
+	@Override
+	public List<Dept> selectAllDept(SqlSession sqlSession) {
+		List<Dept> deptList = sqlSession.selectList("DeptMapper.selectAllDept");
+		return deptList;
+	}
+
+	//부서 선택해서 부서 정보 가져오기
+	@Override
+	public Dept selectDeptInfo(SqlSession sqlSession, String deptCode) {
+		Dept dept = sqlSession.selectOne("DeptMapper.selectDeptInfo", deptCode);
+		return dept;
+	}
+
+	//부서 이름 검색해서 부서 리스트 가져오기
+	@Override
+	public List<Dept> selectDeptSearch(SqlSession sqlSession, String deptSearchName) {
+		List<Dept> deptList = sqlSession.selectList("DeptMapper.selectDeptSearch", deptSearchName);
+		return deptList;
+	}
+
+	//부서 추가
+	@Override
+	public int insertDept(SqlSession sqlSession, Dept dept) {
+		int result = sqlSession.insert("DeptMapper.insertDept", dept);
+		return result;
+	}
+
+	//부서 정보 수정
+	@Override
+	public int updateDept(SqlSession sqlSession, Dept dept) {
+		int result = sqlSession.update("DeptMapper.updateDept", dept);
+		return result;
+	}
+
+	//부서 삭제
+	@Override
+	public int deleteDept(SqlSession sqlSession, String deptCode) {
+		int result = sqlSession.delete("DeptMapper.deleteDept", deptCode);
+		return result;
+	}
 }
