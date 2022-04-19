482be311b8dc9aa994d44c7ac7b2bdf9f5b26a1f
dbdru <dbdru@LEEYG>
2022-04-19 16:51:19 +0900

    0419

--- src/main/java/com/highfive/hirp/employee/service/EmployeeService.java
+++ src/main/java/com/highfive/hirp/employee/service/EmployeeService.java
@@ -1,5 +1,18 @@
 package com.highfive.hirp.employee.service;
 
+import com.highfive.hirp.employee.domain.Employee;
+
 public interface EmployeeService {
 
+	// 회원가입
+	public int registerEmployee(Employee employee);	
+	
+	// 로그인
+	public Employee loginMember(Employee employee);
+
+	// 마이페이지 출력
+	public Employee employeeMyPage(String employeeId);
+	
+	// 비밀번호 찾기
+	public List<Member> find_pwd(HttpServletResponse response, Member member);
 }
