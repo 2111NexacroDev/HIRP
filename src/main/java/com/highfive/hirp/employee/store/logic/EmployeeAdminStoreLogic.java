d0daa38724bece5122b97f171def197cca23aab3
jinsiri <developer_corn@daum.net>
2022-04-19 15:13:26 +0900

    인터페이스 상속 빼먹었던 것 추가

--- src/main/java/com/highfive/hirp/employee/store/logic/EmployeeAdminStoreLogic.java
+++ src/main/java/com/highfive/hirp/employee/store/logic/EmployeeAdminStoreLogic.java
@@ -2,7 +2,9 @@ package com.highfive.hirp.employee.store.logic;
 
 import org.springframework.stereotype.Repository;
 
+import com.highfive.hirp.employee.store.EmployeeAdminStore;
+
 @Repository
-public class EmployeeAdminStoreLogic {
+public class EmployeeAdminStoreLogic implements EmployeeAdminStore {
 
 }
