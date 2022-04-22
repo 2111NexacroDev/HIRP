d7e5b4939c4b088eb32cb5d8cd3fb0a67c5d93f5
woojeong <LG@DESKTOP-Q8IE2R8>
2022-04-22 17:51:17 +0900

    approval 클래스 다이어그램

--- src/main/java/com/highfive/hirp/approval/user/service/ApprovalService.java
+++ src/main/java/com/highfive/hirp/approval/user/service/ApprovalService.java
@@ -1,5 +1,78 @@
 package com.highfive.hirp.approval.user.service;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.highfive.hirp.approval.admin.domain.ApprForm;
+import com.highfive.hirp.approval.user.domain.ApprAccept;
+import com.highfive.hirp.approval.user.domain.Approval;
+import com.highfive.hirp.approval.user.domain.Reference;
+import com.highfive.hirp.common.Search;
+import com.highfive.hirp.group.domain.Group;
+
 public interface ApprovalService {
 
+		//폼 선택 화면
+		public List<ApprForm> printAllApprForm();
+		//폼 검색 조회
+		public List<ApprForm>  printSearchApprForm(Search search);
+		//폼 가져오기(select appr_form)
+		public ApprForm printApprForm(int formNo);
+		
+		//결재자 선택 화면(조직도)(select employee)
+		//public List<Group> printAllGroup();
+		//결재자 검색 조회(select search)
+		//public List<Group> searchAllGroup(Search search);
+		
+		//결재자 선택(insert appr_accept)
+		public int registerApprover(ApprAccept apprAccept);
+		//참조자/열람자 선택(insert reference)
+		public int regitserReference(Reference reference);
+		
+		//문서 상신(insert approval)
+		//TEMPORARY_STORAGE DEFAULT값인 'N'로 들어감
+		public int registerAppr(Approval approval);
+		
+		//임시저장(insert approval)
+		//TEMPORARY_STORAGE 'Y';
+		public int registerStoragedAppr(Approval approval);
+		
+		//임시저장된 문서 수정
+		public int modifyStoragedAppr(int docNo);
+		//임시저장된 문서 삭제
+		public int removeStoragedAppr(int docNo);
+		
+		//결재대기 문서함(select List session에서 id, 진행사항  : 대기)
+		public List<Approval> printAllWaitingAppr(ApprAccept apprAccept); 
+		//결재대기 문서 조회(approval select)
+		public Approval printOneWaitngAppr(int docNo);
+		//결재선 진행 상태 조회(appr_accept select 결재상태 <조건> 문서번호 )
+		public List<ApprAccept> printApprovalStatus(int docNo);
+		//결재자 결재진행(결재승인, 반려)
+		//(appr_accept update 결재상태 "승인,반려" <조건>문서번호,session id값 )
+		public int modifyApprStatus(ApprAccept apprAccept); 
+		//(update approval 진행상태"승인, 진행,반려")
+		public int modifyApprovalStatus(Approval approval);
+		
+		//결재상신 취소
+		//delete approval
+		public int removeApproval(int docNo);
+		//delete appr_accept
+		public int removeApprAccept(int docNo);
+		//상신문서함(select List)
+		public List<Approval> printAllWrittenAppr(ApprAccept apprAccept);
+		//임시저장함(select List)
+		public List<Approval> printAllTemporaryStorageAppr(ApprAccept apprAccept);
+		
+		//반려문서함(select List)
+		public List<Approval> printAllRejectedAppr(ApprAccept apprAccept);
+		
+		//완료문서함(select List)
+		public List<Approval> printAllCompletedAppr(ApprAccept apprAccept);
+		//문서조회(select)
+		public Approval printOneAppr(int docNo);
+	
 }
--- src/main/java/com/highfive/hirp/approval/user/service/logic/ApprovalServiceImpl.java
+++ src/main/java/com/highfive/hirp/approval/user/service/logic/ApprovalServiceImpl.java
@@ -1,5 +1,151 @@
 package com.highfive.hirp.approval.user.service.logic;
 
-public class ApprovalServiceImpl {
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.highfive.hirp.approval.admin.domain.ApprForm;
+import com.highfive.hirp.approval.user.domain.ApprAccept;
+import com.highfive.hirp.approval.user.domain.Approval;
+import com.highfive.hirp.approval.user.domain.Reference;
+import com.highfive.hirp.approval.user.service.ApprovalService;
+import com.highfive.hirp.approval.user.store.ApprovalStore;
+import com.highfive.hirp.common.Search;
+
+@Service
+public class ApprovalServiceImpl implements ApprovalService{
+
+	@Autowired
+	private ApprovalStore aStore;
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<ApprForm> printAllApprForm() {
+		List<ApprForm> aList = aStore.selectAllApprForm(sqlSession);
+		return aList;
+	}
+
+	@Override
+	public List<ApprForm> printSearchApprForm(Search search) {
+		List<ApprForm> aList = aStore.selectSearchApprForm(sqlSession,search);
+		return aList;
+	}
+
+	@Override
+	public ApprForm printApprForm(int formNo) {
+		ApprForm apprForm = aStore.selectApprForm(sqlSession,formNo);
+		return apprForm;
+	}
+
+	@Override
+	public int registerApprover(ApprAccept apprAccept) {
+		int result = aStore.insertApprover(sqlSession,apprAccept);
+		return result;
+	}
+
+	@Override
+	public int regitserReference(Reference reference) {
+		int result = aStore.insertReference(sqlSession,reference);
+		return result;
+	}
+
+	@Override
+	public int registerAppr(Approval approval) {
+		int result = aStore.insertAppr(sqlSession,approval);
+		return result;
+	}
+
+	@Override
+	public int registerStoragedAppr(Approval approval) {
+		int result = aStore.insertStoragedAppr(sqlSession,approval);
+		return result;
+	}
+
+	@Override
+	public int modifyStoragedAppr(int docNo) {
+		int result = aStore.updateStoragedAppr(sqlSession,docNo);
+		return result;
+	}
+
+	@Override
+	public int removeStoragedAppr(int docNo) {
+		int result = aStore.deleteStoragedAppr(sqlSession,docNo);
+		return result;
+	}
+
+	@Override
+	public List<Approval> printAllWaitingAppr(ApprAccept apprAccept) {
+		List<Approval> aList = aStore.selectAllWaitingAppr(sqlSession,apprAccept);
+		return aList;
+	}
+
+	@Override
+	public Approval printOneWaitngAppr(int docNo) {
+		Approval approval = aStore.selectOneWaitingAppr(sqlSession,docNo);
+		return approval;
+	}
+
+	@Override
+	public List<ApprAccept> printApprovalStatus(int docNo) {
+		List<ApprAccept> aList = aStore.selectApprovalStatus(sqlSession,docNo);
+		return aList;
+	}
+
+	@Override
+	public int modifyApprStatus(ApprAccept apprAccept) {
+		int result = aStore.updateApprStatus(sqlSession,apprAccept);
+		return result;
+	}
+
+	@Override
+	public int modifyApprovalStatus(Approval approval) {
+		int result = aStore.updateApprovalStatus(sqlSession,approval);
+		return result;
+	}
+
+	@Override
+	public int removeApproval(int docNo) {
+		int result = aStore.deleteApproval(sqlSession,docNo);
+		return result;
+	}
+
+	@Override
+	public int removeApprAccept(int docNo) {
+		int result = aStore.deleteApprAccept(sqlSession,docNo);
+		return result;
+	}
+
+	@Override
+	public List<Approval> printAllWrittenAppr(ApprAccept apprAccept) {
+		List<Approval> aList = aStore.selectAllWrittenAppr(sqlSession,apprAccept);
+		return aList;
+	}
+
+	@Override
+	public List<Approval> printAllTemporaryStorageAppr(ApprAccept apprAccept) {
+		List<Approval> aList= aStore.selectAllTemporaryStorageAppr(sqlSession,apprAccept);
+		return aList;
+	}
+
+	@Override
+	public List<Approval> printAllRejectedAppr(ApprAccept apprAccept) {
+		List<Approval> aList= aStore.selectAllRejectedAppr(sqlSession,apprAccept);
+		return aList;
+	}
+
+	@Override
+	public List<Approval> printAllCompletedAppr(ApprAccept apprAccept) {
+		List<Approval> aList= aStore.selectAllCompletedAppr(sqlSession,apprAccept);
+		return aList;
+	}
+	@Override
+	public Approval printOneAppr(int docNo) {
+		Approval approval = aStore.selectOneAppr(sqlSession,docNo);
+		return approval;
+	}
 
 }
