f6664a1f81eef615344bfa25ac9b113ae3b0a2f1
woojeong <LG@DESKTOP-Q8IE2R8>
2022-04-22 10:15:33 +0900

    mapper생성

--- src/main/java/com/highfive/hirp/approval/user/controller/ApprovalController.java
+++ src/main/java/com/highfive/hirp/approval/user/controller/ApprovalController.java
@@ -2,7 +2,28 @@ package com.highfive.hirp.approval.user.controller;
 
 public class ApprovalController {
 
-	//
+	//폼 가져오기(select appr_form)
+	
+	//결재자 선택(insert appr_accept)
+	
+	//문서 상신(insert approval)
+	
+	//상신된 문서 조회(select approval)
+	//결재선 진행 상태 조회(select appr_accept)
+	
+	//결재자 결재진행
+	//(update appr_accept)
+	//(update approval)
+	
+	//기안서 회수
+	//delete appr_accept
+	//delete approval
+	
+	
+	
+	
+	
+	
 	
 	
 	
