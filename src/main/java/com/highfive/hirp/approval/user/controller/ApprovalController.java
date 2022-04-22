b1e67f72bfa9d4ccea5fbb5cef8984ed9510b9e7
woojeong <LG@DESKTOP-Q8IE2R8>
2022-04-22 14:32:55 +0900

    오류 수정

--- src/main/java/com/highfive/hirp/approval/user/controller/ApprovalController.java
+++ src/main/java/com/highfive/hirp/approval/user/controller/ApprovalController.java
@@ -4,10 +4,21 @@ public class ApprovalController {
 
 	//폼 가져오기(select appr_form)
 	
+	//결재자 선택 화면(조직도)(select employee)
+	
+	//결재자 검색 조회(select search)
+	
 	//결재자 선택(insert appr_accept)
+	//참조자/열람자 선택(insert reference)
+	
 	
 	//문서 상신(insert approval)
+	//TEMPORARY_STORAGE DEFAULT값인 'N'로 들어감
 	
+	//임시저장(insert approval)
+	//TEMPORARY_STORAGE 'Y';
+	
+
 	//상신된 문서 조회(select approval)
 	//결재선 진행 상태 조회(select appr_accept)
 	
@@ -20,6 +31,16 @@ public class ApprovalController {
 	//delete approval
 	
 	
+
+	//상신문서함(select List)
+	
+	//임시저장함(select List)
+	
+	//반료문서함(select List)
+	
+	//완료문서함(select List)
+	
+	//결재대기 문서함(select List)
 	
 	
 	
