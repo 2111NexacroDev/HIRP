696bfee4f51a3278d0ed01e99160d3ad35980c9d
jinsil <developer_corn@daum.net>
2022-05-17 18:55:43 +0900

    넥사크로 임시회원 상세보기 추가

--- src/main/webapp/nexaui/Application_Desktop.xadl.js
+++ src/main/webapp/nexaui/Application_Desktop.xadl.js
@@ -8,7 +8,9 @@
 			// global dataobject
 		
             // global dataset
-
+            obj = new Dataset("gds_menu", this);
+            obj._setContents("<ColumnInfo><Column id=\"MENU_ID\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_NAME\" type=\"STRING\" size=\"256\"/><Column id=\"MENU_LEVEL\" type=\"STRING\" size=\"256\"/><Column id=\"FORM_URL\" type=\"STRING\" size=\"256\"/></ColumnInfo><Rows><Row><Col id=\"MENU_ID\">10</Col><Col id=\"MENU_NAME\">홈</Col><Col id=\"MENU_LEVEL\">0</Col><Col id=\"FORM_URL\">FrameBase::form_home.xfdl</Col></Row><Row><Col id=\"MENU_ID\">20</Col><Col id=\"MENU_NAME\">사원관리</Col><Col id=\"MENU_LEVEL\">0</Col><Col id=\"FORM_URL\">FrameBase::emplList.xfdl</Col></Row><Row><Col id=\"MENU_ID\">30</Col><Col id=\"MENU_NAME\">부서관리</Col><Col id=\"MENU_LEVEL\">0</Col><Col id=\"FORM_URL\">FrameBase::deptManage.xfdl</Col></Row><Row><Col id=\"MENU_ID\">40</Col><Col id=\"MENU_NAME\">직급관리</Col><Col id=\"MENU_LEVEL\">0</Col></Row><Row><Col id=\"MENU_ID\">50</Col><Col id=\"MENU_NAME\">전자결재관리</Col><Col id=\"MENU_LEVEL\">0</Col></Row><Row><Col id=\"MENU_ID\">60</Col><Col id=\"MENU_NAME\">근태관리</Col><Col id=\"MENU_LEVEL\">0</Col></Row></Rows>");
+            this._addDataset(obj.name, obj);
             
             // global variable
 
@@ -30,7 +32,7 @@
             var mainframe = this.createMainFrame("mainframe","0","0","1280","720",null,null,this);
             mainframe.set_showtitlebar("true");
             mainframe.set_showstatusbar("true");
-            mainframe.set_titletext("FullFrame");
+            mainframe.set_titletext("LeftFrame");
             mainframe.on_createBodyFrame = this.mainframe_createBodyFrame;        
             // tray
 
@@ -43,13 +45,24 @@
         
         this.mainframe_createBodyFrame = function()
         {
-            var frame0 = new ChildFrame("WorkFrame",null,null,null,null,null,null,"techtip::richtexteditor_sample.xfdl",this);
-            frame0.set_showtitlebar("false");
-            frame0.set_showstatusbar("false");
+            var frame0 = new HFrameSet("HFrameSet00",null,null,null,null,null,null,this);
+            frame0.set_separatesize("200,*");
             this.addChild(frame0.name, frame0);
-            frame0.set_formurl("techtip::richtexteditor_sample.xfdl");
-
             this.frame=frame0;
+
+            var frame1 = new ChildFrame("LeftFrame",null,null,null,null,null,null,"FrameBase::Form_Left.xfdl",frame0);
+            frame1.set_showtitlebar("false");
+            frame1.set_showstatusbar("false");
+            frame1.set_showtitleicon("false");
+            frame0.addChild(frame1.name, frame1);
+            frame1.set_formurl("FrameBase::Form_Left.xfdl");
+
+
+            var frame2 = new ChildFrame("WorkFrame",null,null,null,null,null,null,"FrameBase::Form_Work.xfdl",frame0);
+            frame2.set_showtitlebar("false");
+            frame2.set_showtitleicon("false");
+            frame0.addChild(frame2.name, frame2);
+            frame2.set_formurl("FrameBase::Form_Work.xfdl");
         };
         
         this.on_initEvent = function()
@@ -62,7 +75,7 @@
         this.checkLicense("");
         
         this.loadPreloadList();
-        this.loadCss("xcssrc::richtexteditor.xcss");
+
         this.loadIncludeScript("Application_Desktop.xadl");
     };
 }
