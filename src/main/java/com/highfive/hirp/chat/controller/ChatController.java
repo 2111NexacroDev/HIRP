a6a8525aeb7047f5a7a2c2d254cab0006143b064
MING <devel.moon17@gmail.com>
2022-05-22 20:29:36 +0900

    세션

--- src/main/java/com/highfive/hirp/chat/controller/ChatController.java
+++ src/main/java/com/highfive/hirp/chat/controller/ChatController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -33,7 +34,9 @@ public class ChatController {
 	public String view_chat(Model model
 			, HttpServletRequest request
 			, HttpServletResponse response) throws Exception {
-
+		HttpSession session = request.getSession();
+		String emplId = session.getAttribute("emplId").toString();
+		
 		return "chat/chatTestPage";
 	}
 		
