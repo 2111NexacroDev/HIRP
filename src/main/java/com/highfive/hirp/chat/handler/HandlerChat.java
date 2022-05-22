b019300240cd4e39ee27e3611547a5cb1912a880
MING <devel.moon17@gmail.com>
2022-05-22 20:15:16 +0900

    채팅방 만들기 하는 중......

--- src/main/java/com/highfive/hirp/chat/handler/HandlerChat.java
+++ src/main/java/com/highfive/hirp/chat/handler/HandlerChat.java
@@ -1,7 +1,9 @@
 package com.highfive.hirp.chat.handler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -9,33 +11,70 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 public class HandlerChat extends TextWebSocketHandler {
 
-	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
-
-	// 클라이언트가 서버로 연결 처리
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
-		// 채팅방에 접속한 사용자 세션을 리스트에 저장
-		sessionList.add(session);
-
-		// 모든 세션에 채팅 전달
-		for (int i = 0; i < sessionList.size(); i++) {
-			WebSocketSession s = sessionList.get(i);
-			s.sendMessage(new TextMessage(session.getId() + "님이 입장 했습니다."));
-		}
-	}
-
+	// (<"bang_id", 방ID>, <"session", 세션>) - (<"bang_id", 방ID>, <"session", 세션>) - (<"bang_id", 방ID>, <"session", 세션>) 형태 
+	private List<Map<String, Object>> sessionList = new ArrayList<Map<String, Object>>();
+	
 	// 클라이언트가 서버로 메세지 전송 처리
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 
-		// 모든 세션에 채팅 전달
-		for (int i = 0; i < sessionList.size(); i++) {
-			WebSocketSession s = sessionList.get(i);
-			s.sendMessage(new TextMessage(session.getId() + " : " + message.getPayload()));
+		super.handleTextMessage(session, message);
+        
+		// JSON --> Map으로 변환
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, String> mapReceive = objectMapper.readValue(message.getPayload(), Map.class);
+
+		switch (mapReceive.get("cmd")) {
+		
+		// CLIENT 입장
+		case "CMD_ENTER":
+			// 세션 리스트에 저장
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("bang_id", mapReceive.get("bang_id"));
+			map.put("session", session);
+			sessionList.add(map);
+			
+			// 같은 채팅방에 입장 메세지 전송
+			for (int i = 0; i < sessionList.size(); i++) {
+				Map<String, Object> mapSessionList = sessionList.get(i);
+				String bang_id = (String) mapSessionList.get("bang_id");
+				WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
+				
+				if(bang_id.equals(mapReceive.get("bang_id"))) {
+					Map<String, String> mapToSend = new HashMap<String, String>();
+					mapToSend.put("bang_id", bang_id);
+					mapToSend.put("cmd", "CMD_ENTER");
+					mapToSend.put("msg", session.getId() +  "님이 입장 했습니다.");
+					System.out.println("입장메세지 전송"+mapToSend);
+					String jsonStr = objectMapper.writeValueAsString(mapToSend);
+					sess.sendMessage(new TextMessage(jsonStr));
+				}
+			}
+			break;
+			
+		// CLIENT 메세지
+		case "CMD_MSG_SEND":
+			// 같은 채팅방에 메세지 전송
+			for (int i = 0; i < sessionList.size(); i++) {
+				Map<String, Object> mapSessionList = sessionList.get(i);
+				String bang_id = (String) mapSessionList.get("bang_id");
+				WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
+
+				if (bang_id.equals(mapReceive.get("bang_id"))) {
+					Map<String, String> mapToSend = new HashMap<String, String>();
+					mapToSend.put("bang_id", bang_id);
+					mapToSend.put("cmd", "CMD_MSG_SEND");
+					mapToSend.put("msg", session.getId() + " : " + mapReceive.get("msg"));
+					System.out.println("채팅방에 메세지 전송"+mapToSend);
+					String jsonStr = objectMapper.writeValueAsString(mapToSend);
+					sess.sendMessage(new TextMessage(jsonStr));
+				}
+			}
+			break;
 		}
 	}
 
@@ -43,14 +82,39 @@ public class HandlerChat extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 
-		// 채팅방에서 퇴장한 사용자 세션을 리스트에서 제거
-		sessionList.remove(session);
-
-		// 모든 세션에 채팅 전달
+		super.afterConnectionClosed(session, status);
+        
+		ObjectMapper objectMapper = new ObjectMapper();
+		String now_bang_id = "";
+		
+		// 사용자 세션을 리스트에서 제거
 		for (int i = 0; i < sessionList.size(); i++) {
-			WebSocketSession s = sessionList.get(i);
-			s.sendMessage(new TextMessage(session.getId() + "님이 퇴장 했습니다."));
+			Map<String, Object> map = sessionList.get(i);
+			String bang_id = (String) map.get("bang_id");
+			WebSocketSession sess = (WebSocketSession) map.get("session");
+			
+			if(session.equals(sess)) {
+				now_bang_id = bang_id;
+				sessionList.remove(map);
+				break;
+			}	
 		}
+		
+		// 같은 채팅방에 퇴장 메세지 전송 
+		for (int i = 0; i < sessionList.size(); i++) {
+			Map<String, Object> mapSessionList = sessionList.get(i);
+			String bang_id = (String) mapSessionList.get("bang_id");
+			WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
 
+			if (bang_id.equals(now_bang_id)) {
+				Map<String, String> mapToSend = new HashMap<String, String>();
+				mapToSend.put("bang_id", bang_id);
+				mapToSend.put("cmd", "CMD_EXIT");
+				mapToSend.put("msg", session.getId() + "님이 퇴장 했습니다.");
+
+				String jsonStr = objectMapper.writeValueAsString(mapToSend);
+				sess.sendMessage(new TextMessage(jsonStr));
+			}
+		}
 	}
 }
--- src/main/webapp/WEB-INF/views/chat/chatTestPage.jsp
+++ src/main/webapp/WEB-INF/views/chat/chatTestPage.jsp
@@ -14,11 +14,29 @@
 				this._initSocket();
 			},
 			sendChat: function() {
-				this._sendMessage($('#message').val());
+				this._sendMessage('${param.bang_id}', 'CMD_MSG_SEND', $('#message').val());
+				$('#message').val('');
+				console.log("${param.bang_id}");
+				console.log($('#message').val(''));
+			},
+			sendEnter: function() {
+				this._sendMessage('${param.bang_id}', 'CMD_ENTER', $('#message').val());
 				$('#message').val('');
 			},
-			receiveMessage: function(str) {
-				$('#divChatData').append('<div>' + str + '</div>');				
+			receiveMessage: function(msgData) {
+
+				// 정의된 CMD 코드에 따라서 분기 처리
+				if(msgData.cmd == 'CMD_MSG_SEND') {					
+					$('#divChatData').append('<div>' + msgData.msg + '</div>');
+				}
+				// 입장
+				else if(msgData.cmd == 'CMD_ENTER') {
+					$('#divChatData').append('<div>' + msgData.msg + '</div>');
+				}
+				// 퇴장
+				else if(msgData.cmd == 'CMD_EXIT') {					
+					$('#divChatData').append('<div>' + msgData.msg + '</div>');
+				}
 			},
 			closeMessage: function(str) {
 				$('#divChatData').append('<div>' + '연결 끊김 : ' + str + '</div>');
@@ -28,19 +46,30 @@
 			},
 			_initSocket: function() {
 				this._socket = new SockJS(this._url);
+				this._socket.onopen = function(evt) {
+					webSocket.sendEnter();
+				};
 				this._socket.onmessage = function(evt) {
-					webSocket.receiveMessage(evt.data);
+					webSocket.receiveMessage(JSON.parse(evt.data));
 				};
 				this._socket.onclose = function(evt) {
-					webSocket.closeMessage(evt.data);
+					webSocket.closeMessage(JSON.parse(evt.data));
 				}
 			},
-			_sendMessage: function(str) {
-				this._socket.send(str);
+			_sendMessage: function(bang_id, cmd, msg) {
+				var msgData = {
+						bang_id : bang_id,
+						cmd : cmd,
+						msg : msg
+				};
+				var jsonData = JSON.stringify(msgData);
+				this._socket.send(jsonData);
 			}
 		};
-		$(document).ready(function() {
-			webSocket.init({ url: '<c:url value="/chat" />' });			
+	</script>	
+	<script type="text/javascript">
+        $(window).on('load', function () {
+			webSocket.init({ url: '<c:url value="/chat" />' });	
 		});
 	</script>
 </head>
