814836a47270f095a1f107a05aff5f23f23dbf7a
ming <devel.moon17@gmail.com>
2022-05-23 09:49:05 +0900

    dd

--- src/main/java/com/highfive/hirp/schedule/store/ScheduleStore.java
+++ src/main/java/com/highfive/hirp/schedule/store/ScheduleStore.java
@@ -13,11 +13,14 @@ public interface ScheduleStore {
 	public List<Schedule> selectAllSchedule(SqlSession sqlSession, String loginUser);
 	public List<Schedule> selectSearchSchedule(SqlSession sqlSession, Search search);
 	
+	int insertSchedule(SqlSession sqlSession, Schedule schedule);
 	public int insertCompanySchedule(SqlSession sqlSession, Schedule schedule);
 	public int insertTeamSchedule(SqlSession sqlSession, Schedule schedule);
 	public int insertScheduleToSub(SqlSession sqlSession, Schedule schedule);
 	public int insertPersonalSchedule(SqlSession sqlSession, Schedule schedule);
 	
 	public int updateSchedule(SqlSession sqlSession, Schedule schedule);
+	int updateCompanySchedule(SqlSession sqlSession, Schedule schedule);
 	public int deleteSchedule(SqlSession sqlSession, int scheduleNo);
+	int deleteCompanySchedule(SqlSession sqlSession, int scheduleNo);
 }
--- src/main/java/com/highfive/hirp/schedule/store/logic/ScheduleStoreLogic.java
+++ src/main/java/com/highfive/hirp/schedule/store/logic/ScheduleStoreLogic.java
@@ -40,6 +40,11 @@ public class ScheduleStoreLogic implements ScheduleStore {
 		int result = sqlSession.insert("ScheduleMapper.insertSchedule", schedule);
 		return result;
 	}
+	@Override
+	public int insertTeamSchedule(SqlSession sqlSession, Schedule schedule) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
 	public int insertScheduleToSub(SqlSession sqlSession, Schedule schedule) {
@@ -51,6 +56,12 @@ public class ScheduleStoreLogic implements ScheduleStore {
 	public int insertCompanySchedule(SqlSession sqlSession, Schedule schedule) {
 		int result = sqlSession.insert("ScheduleMapper.insertCompanySchedule", schedule);
 		return result;
+	}
+	
+	@Override
+	public int insertPersonalSchedule(SqlSession sqlSession, Schedule schedule) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 	@Override
@@ -76,4 +87,5 @@ public class ScheduleStoreLogic implements ScheduleStore {
 		int result = sqlSession.delete("ScheduleMapper.deleteCompanySchedule", scheduleNo);
 		return result;
 	}
+
 }
